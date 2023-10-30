fd2b2c69a9cb80799a2ddaf93dbcead4da9abb38
ikaruuskisia <149347275+ikaruuskisia@users.noreply.github.com>
2023-10-29 21:11:58 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/a/macros/cecyt12.edu.mx/s/AKfycbw_mKP4rn0BWrqg5-pYGqWAEg-NFfX-WU5gJA7Bf4PG9O2Nu-tAnCCO9MTBM5_iYbuzlg/exec";
